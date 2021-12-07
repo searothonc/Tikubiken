ee2f3c83322bd36a7a0c8334a78ce37237a632fb
searothonc <searothonc+github@gmail.com>
2021-12-08 08:45:01 +0900

    IDE generated original source.

--- Update/Program.cs
+++ Update/Program.cs
@@ -6,18 +6,18 @@ using System.Windows.Forms;
 
 namespace Tikubiken
 {
-    static class Program
-    {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormPatch());
-        }
-    }
+	static class Program
+	{
+		/// <summary>
+		///  The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.SetHighDpiMode(HighDpiMode.SystemAware);
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new FormPatch());
+		}
+	}
 }
