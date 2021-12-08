c84bd88a49992756c513e2e2a8917be11c194ef6
searothonc <searothonc+github@gmail.com>
2021-12-08 10:55:08 +0900

    Snapshot for test code to taking proper executable directory. Found it in Sysytem.Diagnotics.Process class.

--- Update/Program.cs
+++ Update/Program.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.Diagnostics;
+//using System.Management;
+
 namespace Tikubiken
 {
 	static class Program
@@ -14,10 +17,52 @@ namespace Tikubiken
 		[STAThread]
 		static void Main()
 		{
+			ExtractData();
+			ShowForm();
+		}
+
+		//--------------------------------------------------------
+		// Windows form
+		//--------------------------------------------------------
+		static void ShowForm()
+		{
 			Application.SetHighDpiMode(HighDpiMode.SystemAware);
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			Application.Run(new FormPatch());
 		}
+
+		//--------------------------------------------------------
+		// Extract data from executable file
+		//--------------------------------------------------------
+		private static void ExtractData()
+		{
+			var proc = Process.GetCurrentProcess();
+			MessageBox.Show(proc.MainModule.FileName, "TEST", MessageBoxButtons.OK, MessageBoxIcon.Information);
+		}
+
+		//--------------------------------------------------------
+		// Get parent process info
+		//--------------------------------------------------------
+/*
+		private static Process GetParentProcess()
+		{
+			var procId = Process.GetCurrentProcess().Id;
+			string query = $"SELECT ParentProcessId FROM Win32_Process WHERE ProcessId={procId}";
+			int parentProcId;
+
+			using ( var s = new ManagementObjectSearcher(@"root\CIMV2", query) )
+			using ( var results = s.Get().GetEnumerator() )
+			{
+
+				if (!results.MoveNext()) throw new ApplicationException("Couldn't Get ParrentProcessId.");
+
+				var queryResult = results.Current;
+				parentProcId = (int) queryResult["ParentProcessId"];
+			}
+
+			return Process.GetProcessById( parentProcId );
+		}
+*/
 	}
 }
